b7d42b2dbaa87d5d99780e38b25190b5546ccccf
Erikgavs <erikgavs@gmail.com>
2026-09-09 15:40:13 +0200

    App construction and setup function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    // Aplication Startup
+    App::new()
+        .add_plugins(DefaultPlugins) // Getting all the basic features of beavy w this line
+        .add_systems(Startup, setup)
+        .run();
+}
+
+// bevy will call this once when we run the code
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera3d::default()); // adds a camera to the scene. Just to be able to see the dice
 }
