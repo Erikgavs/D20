dfdf6a48b78a1ddf8d4217dde50ff86690f1164d
Erikgavs <erikgavs@gmail.com>
2026-09-11 22:17:13 +0200

    Added some comments to make more clear the code

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
 use bevy::prelude::*;
 
 fn main() {
-    // Aplication Startup
+    // Application startup
     App::new()
-        .add_plugins(DefaultPlugins) // Getting all the basic features of beavy w this line
+        .add_plugins(DefaultPlugins) // gets all the basic features of bevy with this line
         .add_systems(Startup, setup)
         .run();
 }
 
-// bevy will call this once when we run the code
-fn setup(mut commands: Commands) {
+// Bevy will call this once when we run the code.
+// Startup runs once: here we build the initial scene (camera, light, later the dice).
+//
+// mut meshes: ResMut<Assets<Mesh>> -> shape box: stores the d20's geometry
+//
+// ResMut -> mutable access to the resource: lets us save our d20 dice (geometrical form)
+//
+// mut materials: ResMut<Assets<StandardMaterial>> -> paint box: stores the materials to fill/paint the shapes
+//
+// commands are essential: the spawned entities (camera, light) exist for the whole app
+fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<StandardMaterial>>) {
     commands.spawn(Camera3d::default()); // adds a camera to the scene. Just to be able to see the dice
+    commands.spawn(DirectionalLight::default());
 }
